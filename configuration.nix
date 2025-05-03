3675e3bcbc5b9aff8edceb1d30c3387da3140d67
SweetCapitan <danil.duhanin@yandex.ru>
2025-05-03 14:52:50 +0300

    chore: add Wireshark and update sing-box compatibility
    - Add Wireshark to system packages for network analysis
    - Fix configuration to support newer sing-box versions

--- configuration.nix
+++ configuration.nix
@@ -16,9 +16,10 @@
     ./hardware-configuration.nix
     ./nvim/neovim.nix
     ./syncthings.nix
-    #TODO: search reason for the problem in the sing-box after 1.9
-    ./sing-box-override.nix
+    #currently not needed :D
+    #./sing-box-override.nix
     ./common/network/tele2_ttl_change.nix
+    ./avahi.nix
   ];
 
   #-------------------------------------------
@@ -40,6 +41,7 @@
     trustedInterfaces = [ "sing-box-tun" ];
     logRefusedPackets = true;
     logRefusedConnections = true;
+    checkReversePath = "loose";
     extraCommands = ''
       iptables -A INPUT -j LOG --log-prefix "FIREWALL INPUT: "
       iptables -A FORWARD -j LOG --log-prefix "FIREWALL FORWARD: "
@@ -122,6 +124,7 @@
       options = "--delete-older-than 30d";
     };
   };
+
   environment.systemPackages = with pkgs; [
     nixfmt-rfc-style
     sing-box # TODO: remove /usr/share/sing-box/geoip.db
@@ -136,6 +139,7 @@
     lazygit
 
     spotify
+    wireshark
   ];
   #programs.nixvim.enable = true;
   #programs.neovim = {
@@ -237,53 +241,6 @@
     ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
