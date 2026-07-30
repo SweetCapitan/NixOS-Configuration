30835fff8928e4602b8371a4934eef4959ea47f7
SweetCapitan <danil.duhanin@yandex.ru>
2026-07-30 12:42:57 +0300

    fix llama cpp overlay and bump version

--- llama-cuda-overlay.nix
+++ llama-cuda-overlay.nix
@@ -10,31 +10,28 @@ let
     config.cudaCapabilities = [ "6.1" ];
     config.allowUnsupportedSystem = true;
     config.cudaForwardCompat = false;
-    # config.cudaPackages = "cudaPackages_12_9";
   };
 
-  cudaPackages = prev.cudaPackages_12_9.overrideScope (
+  cudaPackages = pkgs_unstable_small.cudaPackages_12.overrideScope (
     cFinal: cPrev: {
       flags = cPrev.flags // {
         cmakeCudaArchitecturesString = "61";
       };
+      cuda_compat = null;
     }
   );
 in
 {
   llama-cpp =
     (pkgs_unstable_small.llama-cpp.override {
-      cudaSupport = true; # explicit override
-      cudaPackages = cudaPackages;
+      cudaSupport = true;
+      inherit cudaPackages;
     }).overrideAttrs
       (old: {
         cmakeFlags = (builtins.filter (f: builtins.match ".*GGML_NATIVE.*" f == null) old.cmakeFlags) ++ [
           "-DGGML_NATIVE=ON"
-          # "-DGGML_BACKEND_DL=ON"
-          # "-DGGML_CPU_ALL_VARIANTS=ON"
           "-DGGML_LTO=ON"
         ];
-        # Disable Nix's march=native stripping
         preConfigure = ''
           export NIX_ENFORCE_NO_NATIVE=0
           ${old.preConfigure or ""}
